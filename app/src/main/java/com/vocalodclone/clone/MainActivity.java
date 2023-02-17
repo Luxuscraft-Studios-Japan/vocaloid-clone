4ee548c344afd2ed0a5d05c287efa63f2d4d5e3b
justinweichTV <64624657+justinweichTV@users.noreply.github.com>
2023-02-17 17:50:38 +0100

    Beta
    This is the very simple and a beta version of our clone

---
+++ app/src/main/java/com/vocalodclone/clone/MainActivity.java
@@ -0,0 +1,193 @@
+package com.vocalodclone.clone;
+
+import android.animation.*;
+import android.app.*;
+import android.content.*;
+import android.content.Intent;
+import android.content.res.*;
+import android.graphics.*;
+import android.graphics.drawable.*;
+import android.media.*;
+import android.media.MediaPlayer;
+import android.net.*;
+import android.net.Uri;
+import android.os.*;
+import android.text.*;
+import android.text.style.*;
+import android.util.*;
+import android.view.*;
+import android.view.View.*;
+import android.view.animation.*;
+import android.webkit.*;
+import android.widget.*;
+import android.widget.LinearLayout;
+import androidx.annotation.*;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
+import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import com.google.android.material.appbar.AppBarLayout;
+import java.io.*;
+import java.text.*;
+import java.util.*;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.regex.*;
+import org.json.*;
+
+public class MainActivity extends AppCompatActivity {
+	
+	private Timer _timer = new Timer();
+	
+	private Toolbar _toolbar;
+	private AppBarLayout _app_bar;
+	private CoordinatorLayout _coordinator;
+	
+	private LinearLayout linear2;
+	private VideoView videoview1;
+	
+	private MediaPlayer h;
+	private TimerTask wb;
+	private Intent next = new Intent();
+	
+	@Override
+	protected void onCreate(Bundle _savedInstanceState) {
+		super.onCreate(_savedInstanceState);
+		setContentView(R.layout.main);
+		initialize(_savedInstanceState);
+		initializeLogic();
+	}
+	
+	private void initialize(Bundle _savedInstanceState) {
+		_app_bar = findViewById(R.id._app_bar);
+		_coordinator = findViewById(R.id._coordinator);
+		_toolbar = findViewById(R.id._toolbar);
+		setSupportActionBar(_toolbar);
+		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+		getSupportActionBar().setHomeButtonEnabled(true);
+		_toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View _v) {
+				onBackPressed();
+			}
+		});
+		linear2 = findViewById(R.id.linear2);
+		videoview1 = findViewById(R.id.videoview1);
+		MediaController videoview1_controller = new MediaController(this);
+		videoview1.setMediaController(videoview1_controller);
+	}
+	
+	private void initializeLogic() {
+		if (Build.VERSION.SDK_INT > Build.VERSION_CODES.KITKAT) {
+			Window w =MainActivity.this.getWindow();
+			w.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+			w.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS); w.setStatusBarColor(0xFF000000);
+		}
+		final java.io.File videoview1F = new java.io.File(getCacheDir(), "logo.mp4");
+		if (!videoview1F.exists()) {
+			try {
+				java.io.InputStream videoview1asset = getAssets().open("logo.mp4");
+				java.io.FileOutputStream videoview1FOS = null;
+				videoview1FOS = new java.io.FileOutputStream(videoview1F);
+				final byte[] videoview1B = new byte[1024];
+				int videoview1S;
+				while ((videoview1S = videoview1asset.read(videoview1B)) != -1) {
+					videoview1FOS.write(videoview1B, 0, videoview1S);
+				}
+				videoview1asset.close();
+				videoview1FOS.close();
+			} catch (java.io.IOException e) {
+			}
+		}
+		videoview1.setVideoPath(videoview1F.getAbsolutePath());
+		videoview1.start();
+		videoview1.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View _view) {
+				
+			}
+		});
+		videoview1.setOnLongClickListener(new View.OnLongClickListener() {
+			@Override
+			public boolean onLongClick(View _view) {
+				next.setClass(getApplicationContext(), NewpActivity.class);
+				startActivity(next);
+				return true;
+			}
+		});
+		videoview1.setOnTouchListener(new View.OnTouchListener(){
+			@Override
+			public boolean onTouch(View _view, MotionEvent _motionEvent){
+				 
+				return true;
+			}
+		});
+		wb = new TimerTask() {
+			@Override
+			public void run() {
+				runOnUiThread(new Runnable() {
+					@Override
+					public void run() {
+						next.setClass(getApplicationContext(), NewpActivity.class);
+						startActivity(next);
+					}
+				});
+			}
+		};
+		_timer.schedule(wb, (int)(11000));
+	}
+	
+	
+	@Deprecated
+	public void showMessage(String _s) {
+		Toast.makeText(getApplicationContext(), _s, Toast.LENGTH_SHORT).show();
+	}
+	
+	@Deprecated
+	public int getLocationX(View _v) {
+		int _location[] = new int[2];
+		_v.getLocationInWindow(_location);
+		return _location[0];
+	}
+	
+	@Deprecated
+	public int getLocationY(View _v) {
+		int _location[] = new int[2];
+		_v.getLocationInWindow(_location);
+		return _location[1];
+	}
+	
+	@Deprecated
+	public int getRandom(int _min, int _max) {
+		Random random = new Random();
+		return random.nextInt(_max - _min + 1) + _min;
+	}
+	
+	@Deprecated
+	public ArrayList<Double> getCheckedItemPositionsToArray(ListView _list) {
+		ArrayList<Double> _result = new ArrayList<Double>();
+		SparseBooleanArray _arr = _list.getCheckedItemPositions();
+		for (int _iIdx = 0; _iIdx < _arr.size(); _iIdx++) {
+			if (_arr.valueAt(_iIdx))
+			_result.add((double)_arr.keyAt(_iIdx));
+		}
+		return _result;
+	}
+	
+	@Deprecated
+	public float getDip(int _input) {
+		return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, _input, getResources().getDisplayMetrics());
+	}
+	
+	@Deprecated
+	public int getDisplayWidthPixels() {
+		return getResources().getDisplayMetrics().widthPixels;
+	}
+	
+	@Deprecated
+	public int getDisplayHeightPixels() {
+		return getResources().getDisplayMetrics().heightPixels;
+	}
+}
---
+++ app/src/main/java/com/vocalodclone/clone/NewpActivity.java
@@ -0,0 +1,218 @@
+package com.vocalodclone.clone;
+
+import android.animation.*;
+import android.app.*;
+import android.app.AlertDialog;
+import android.content.*;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.res.*;
+import android.graphics.*;
+import android.graphics.drawable.*;
+import android.media.*;
+import android.net.*;
+import android.net.Uri;
+import android.os.*;
+import android.speech.tts.TextToSpeech;
+import android.text.*;
+import android.text.style.*;
+import android.util.*;
+import android.view.*;
+import android.view.View;
+import android.view.View.*;
+import android.view.animation.*;
+import android.webkit.*;
+import android.widget.*;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import androidx.annotation.*;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import java.io.*;
+import java.text.*;
+import java.util.*;
+import java.util.regex.*;
+import org.json.*;
+
+public class NewpActivity extends AppCompatActivity {
+	
+	private LinearLayout linear1;
+	private LinearLayout linear2;
+	private LinearLayout linear3;
+	private LinearLayout linear4;
+	private EditText edittext1;
+	private LinearLayout linear5;
+	private TextView textview4;
+	private Button button1;
+	private Button button2;
+	private Button button3;
+	private TextView textview3;
+	
+	private AlertDialog.Builder dialog;
+	private Intent fu = new Intent();
+	private TextToSpeech synth;
+	
+	@Override
+	protected void onCreate(Bundle _savedInstanceState) {
+		super.onCreate(_savedInstanceState);
+		setContentView(R.layout.newp);
+		initialize(_savedInstanceState);
+		initializeLogic();
+	}
+	
+	private void initialize(Bundle _savedInstanceState) {
+		linear1 = findViewById(R.id.linear1);
+		linear2 = findViewById(R.id.linear2);
+		linear3 = findViewById(R.id.linear3);
+		linear4 = findViewById(R.id.linear4);
+		edittext1 = findViewById(R.id.edittext1);
+		linear5 = findViewById(R.id.linear5);
+		textview4 = findViewById(R.id.textview4);
+		button1 = findViewById(R.id.button1);
+		button2 = findViewById(R.id.button2);
+		button3 = findViewById(R.id.button3);
+		textview3 = findViewById(R.id.textview3);
+		dialog = new AlertDialog.Builder(this);
+		synth = new TextToSpeech(getApplicationContext(), null);
+		
+		textview4.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View _view) {
+				if (synth.isSpeaking()) {
+					synth.stop();
+				}
+				else {
+					synth.speak(edittext1.getText().toString(), TextToSpeech.QUEUE_ADD, null);
+				}
+			}
+		});
+		
+		button1.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View _view) {
+				dialog.setTitle("File");
+				dialog.setMessage("please choose a option ");
+				dialog.setPositiveButton("New", new DialogInterface.OnClickListener() {
+					@Override
+					public void onClick(DialogInterface _dialog, int _which) {
+						fu.setClass(getApplicationContext(), NewpActivity.class);
+						startActivity(fu);
+						finish();
+					}
+				});
+				dialog.setNegativeButton("Change Voicebank", new DialogInterface.OnClickListener() {
+					@Override
+					public void onClick(DialogInterface _dialog, int _which) {
+						dialog.setTitle("Voicebanks");
+						dialog.setMessage("please choose a Voicebank");
+						dialog.setPositiveButton("Kazuko", new DialogInterface.OnClickListener() {
+							@Override
+							public void onClick(DialogInterface _dialog, int _which) {
+								synth.setPitch((float)220);
+								synth.setSpeechRate((float)0);
+								SketchwareUtil.showMessage(getApplicationContext(), "voicebank Kazuko sucssesfully selected!");
+							}
+						});
+						dialog.setNegativeButton("Natsuki", new DialogInterface.OnClickListener() {
+							@Override
+							public void onClick(DialogInterface _dialog, int _which) {
+								synth.setPitch((float)147);
+								synth.setSpeechRate((float)0);
+								SketchwareUtil.showMessage(getApplicationContext(), "voicebank Natsuki sucssesfully selected!");
+							}
+						});
+						dialog.setNeutralButton("Mei", new DialogInterface.OnClickListener() {
+							@Override
+							public void onClick(DialogInterface _dialog, int _which) {
+								synth.setPitch((float)175);
+								synth.setSpeechRate((float)0);
+								SketchwareUtil.showMessage(getApplicationContext(), "voicebank Mei sucssesfully selected!");
+							}
+						});
+						dialog.create().show();
+					}
+				});
+				dialog.setNeutralButton("Save", new DialogInterface.OnClickListener() {
+					@Override
+					public void onClick(DialogInterface _dialog, int _which) {
+						
+					}
+				});
+				dialog.create().show();
+			}
+		});
+		
+		button2.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View _view) {
+				
+			}
+		});
+		
+		button3.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View _view) {
+				
+			}
+		});
+	}
+	
+	private void initializeLogic() {
+	}
+	
+	
+	@Deprecated
+	public void showMessage(String _s) {
+		Toast.makeText(getApplicationContext(), _s, Toast.LENGTH_SHORT).show();
+	}
+	
+	@Deprecated
+	public int getLocationX(View _v) {
+		int _location[] = new int[2];
+		_v.getLocationInWindow(_location);
+		return _location[0];
+	}
+	
+	@Deprecated
+	public int getLocationY(View _v) {
+		int _location[] = new int[2];
+		_v.getLocationInWindow(_location);
+		return _location[1];
+	}
+	
+	@Deprecated
+	public int getRandom(int _min, int _max) {
+		Random random = new Random();
+		return random.nextInt(_max - _min + 1) + _min;
+	}
+	
+	@Deprecated
+	public ArrayList<Double> getCheckedItemPositionsToArray(ListView _list) {
+		ArrayList<Double> _result = new ArrayList<Double>();
+		SparseBooleanArray _arr = _list.getCheckedItemPositions();
+		for (int _iIdx = 0; _iIdx < _arr.size(); _iIdx++) {
+			if (_arr.valueAt(_iIdx))
+			_result.add((double)_arr.keyAt(_iIdx));
+		}
+		return _result;
+	}
+	
+	@Deprecated
+	public float getDip(int _input) {
+		return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, _input, getResources().getDisplayMetrics());
+	}
+	
+	@Deprecated
+	public int getDisplayWidthPixels() {
+		return getResources().getDisplayMetrics().widthPixels;
+	}
+	
+	@Deprecated
+	public int getDisplayHeightPixels() {
+		return getResources().getDisplayMetrics().heightPixels;
+	}
+}
